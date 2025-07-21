31631996393d5b510bca427fc2904cbe128b2984
tungwithconga <147390524+tungwithconga@users.noreply.github.com>
2025-07-21 21:34:35 +0700

    Update bai1.c

--- ss19/bai1.c
+++ ss19/bai1.c
@@ -1,3 +1,41 @@
-//
-// Created by Admin on 21/07/2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode != NULL) {
+        newNode->data = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+    }
+    return newNode;
+}
+
+void preorder(Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main() {
+
+    Node* root = createNode(2);
+    root->left = createNode(3);
+    root->right = createNode(4);
+    root->left->left = createNode(5);
+    root->left->right = createNode(6);
+    root->right->left = createNode(7);
+
+    printf("Preorder traversal of the tree:\n");
+    preorder(root);
+
+    return 0;
+}
